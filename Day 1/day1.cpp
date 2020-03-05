eeb375de505743019e5ad353ada9174e991da8e7
Anshuman-UCSB <Anshuman@ucsb.edu>
2020-03-04 16:46:39 -0800

    individual makefiles

--- Day 1/day1.cpp
+++ Day 1/day1.cpp
@@ -6,5 +6,15 @@
 using namespace std;
 
 int main(){
-    FileReader fRead("day1.txt");
+    ifstream file("day1.txt");
+    FileReader fRead;
+    vector<string> data;
+    data = fRead.read(file);
+    int freq = 0;
+    int change;
+    for(unsigned int i = 0;i<data.size();i++){
+        change = atoi(data[i].c_str());
+        freq += change;
+        cout<<i<<": "<<" -> "<<freq<<endl;
+    }
 }
