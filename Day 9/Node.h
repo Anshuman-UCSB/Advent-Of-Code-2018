8ec0244f6da8e3b552f1cdaed1c4962c9d4de138
Anshuman-UCSB <Anshuman@ucsb.edu>
2020-03-12 02:11:14 -0700

    day 9 p1

--- Day 9/Node.h
+++ Day 9/Node.h
@@ -1,21 +1,48 @@
+#include <iostream>
+using namespace std;
 
 struct Node{
     int number;
     Node* next;
+    Node* last;
     Node(int dat){
         number = dat;
     }
 };
 
-void insert(Node* before, Node* ins){
+void insert(Node* before, Node* ins, int& size){
     ins->next = before->next;
+    ins->next->last = ins;
+    ins->last = before;
     before->next = ins;
+    size++;
 }
 
-int remove(Node* before){
-    Node* rem = before->next;
-    before = rem->next;
+void insert(Node* before, int val, int& size){
+    insert(before, new Node(val), size);
+}
+
+int remove(Node* rem, int& size){
+    rem->last->next = rem->next;
+    rem->next->last = rem->last;
     int ret = rem->number;
     delete rem;
-    return ret;
+    size--;
+    return (ret);
+}
+
+void right(Node*& p){
+    p = p->next;
+}
+
+void left(Node*& p){
+    p = p->last;
+}
+
+void print(Node* head, int size){
+    Node* p = head;
+    for(int i =0;i<size;i++){
+        cout<<p->number<<" ";
+        right(p);
+    }cout<<endl;
 }
--- Day 9/day9.cpp
+++ Day 9/day9.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "Node.h"
 
-#define PLAYERS 9
-#define MARBLE 25
+#define PLAYERS 465
+#define MARBLE 7194000
 
 using namespace std;
 
@@ -13,20 +13,56 @@ void printList(Node* head){
     }cout<<endl;
 }
 
-void partOne(){
-    Node* head = new Node(0);
-    Node* current = head;
-    head->next = head;
-    int player = 0;
-    for(int i = 1;i<MARBLE+1;i++){
-        cout<<"Player: "<<player+1<<endl;
-        printList(head);
-        current = current->next;
-        insert(current, new Node(i));
-        current = current->next;
-        player++;
-        player = player%PLAYERS;
+int highScore(int players[PLAYERS]){
+    int max = -1;
+    
+    for(int i = 0;i<PLAYERS;i++){
+        max = max<players[i] ? players[i] : max;
     }
+
+    return max;
+}
+
+void partOne(){
+    int players[PLAYERS];
+    int currentPlayer = 0;
+    for(int i = 0;i<PLAYERS;i++){
+        players[i] = 0;
+    }
+
+    Node* head = new Node(0);
+    head->last = head;
+    head->next = head;
+    Node* current = head;
+    int size = 1;
+
+    cout<<"Current is "<<current->number<<endl;
+    cout<<"[-] ";
+    print(head, size);
+
+    for(int i = 1;i<=MARBLE;i++){
+
+        if(i%23 == 0){
+            for(int j = 0;j<6;j++)
+                left(current);
+            //cout<<"Removing current "<<current->number<<endl;
+            players[currentPlayer]+= remove(current->last, size);
+            players[currentPlayer]+=i;
+        }
+        else{
+            right(current);
+            insert(current, i, size);
+            right(current);
+        }
+        //cout<<"Current is "<<current->number<<endl;
+        //cout<<"High score: "<<highScore(players)<<endl;
+        //cout<<"["<<currentPlayer+1<<"] ";
+        //print(head, size);
+        currentPlayer++;
+        currentPlayer%=PLAYERS;
+    }
+    cout<<highScore(players)<<endl;
+    
 }
 
 int main(){
