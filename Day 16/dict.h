cb0e0a58323037086a971e7b8ebf731b44a38680
Anshuman-UCSB <Anshuman@ucsb.edu>
2020-03-29 17:41:49 -0700

    Day 16 part 1 solved

--- Day 16/dict.h
+++ Day 16/dict.h
@@ -26,73 +26,120 @@ string dict[16] = {
 };
 
 
-vector<int> addr(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] + reg[reg[2]];
+vector<int> addr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] + reg[cmd[2]];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> addi(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] + reg[2];
+vector<int> addi(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] + cmd[2];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> mulr(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] * reg[reg[2]];
+vector<int> mulr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] * reg[cmd[2]];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> muli(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] * reg[2];
+vector<int> muli(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] * cmd[2];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> banr(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] & reg[reg[2]];
+vector<int> banr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] & reg[cmd[2]];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> bani(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] & reg[2];
+vector<int> bani(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] & cmd[2];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> borr(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] | reg[reg[2]];
+vector<int> borr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] | reg[cmd[2]];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> bori(int reg[4]){
-    reg[reg[3]] = reg[reg[1]] | reg[2];
+vector<int> bori(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] | cmd[2];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> setr(int reg[4]){
-    reg[reg[3]] = reg[reg[1]];
+vector<int> setr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> seti(int reg[4]){
-    reg[reg[3]] = reg[1];
+vector<int> seti(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = cmd[1];
     return vector<int>{reg[0],reg[1],reg[2],reg[3]};
 }
 
-vector<int> eval(int reg[4], int opId){
-    string opInstr = dict[opId];
-    switch(opId){
-        case 0: return addr(reg);
-        case 1: return addi(reg);
-        case 2: return mulr(reg);
-        case 3: return muli(reg);
-        case 4: return banr(reg);
-        case 5: return bani(reg);
-        case 6: return borr(reg);
-        case 7: return bori(reg);
-        case 8: return setr(reg);
-        case 9: return seti(reg);
+vector<int> gtir(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = cmd[1] > reg[cmd[2]];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+vector<int> gtri(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] > cmd[2];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+vector<int> gtrr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] > reg[cmd[2]];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+vector<int> eqir(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = cmd[1] == reg[cmd[2]];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+vector<int> eqri(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] == cmd[2];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+vector<int> eqrr(vector<int> reg, vector<int> cmd){
+    reg[cmd[3]] = reg[cmd[1]] == reg[cmd[2]];
+    return vector<int>{reg[0],reg[1],reg[2],reg[3]};
+}
+
+
+vector<int> eval(vector<int> reg, vector<int> cmd, int opCode){
+    string opInstr = dict[cmd[0]];
+    switch(opCode){
+        case 0: return addr(reg, cmd);
+        case 1: return addi(reg, cmd);
+        case 2: return mulr(reg, cmd);
+        case 3: return muli(reg, cmd);
+        case 4: return banr(reg, cmd);
+        case 5: return bani(reg, cmd);
+        case 6: return borr(reg, cmd);
+        case 7: return bori(reg, cmd);
+        case 8: return setr(reg, cmd);
+        case 9: return seti(reg, cmd);
+        case 10:return gtir(reg, cmd);
+        case 11:return gtri(reg, cmd);
+        case 12:return gtrr(reg, cmd);
+        case 13:return eqir(reg, cmd);
+        case 14:return eqri(reg, cmd);
+        case 15:return eqrr(reg, cmd);
     }
 }
 
-
+bool workingOpCode(vector<int> reg, vector<int> cmd, vector<int> exp, int opC){
+    // cout<<"Evaluating for "<<dict[opC]<<endl;
+    vector<int> out = eval(reg,cmd, opC);
+    for(int i = 0;i<4;i++){
+        if(out[i] != exp[i]){
+            return false;
+        }
+    }
+    // cout<<dict[opC]<<" is valid!"<<endl;
+    return true;
+}
 
 
 #endif
--- Day 16/main.cpp
+++ Day 16/main.cpp
@@ -12,8 +12,11 @@ int main(){
 }
 
 int valid(vector<string> set){
+    // for(int i = 0;i<set.size();i++){
+    //     cout<<set[i]<<endl;
+    // }
     int out = 0;
-    int reg[4] = {-1,-1,-1,-1};
+    vector<int> reg(4);
     string vals = set[0];
     vals = vals.substr(9);
     vals[vals.size()-1] = ',';
@@ -21,12 +24,22 @@ int valid(vector<string> set){
         reg[i] = stoi(vals.substr(0,vals.find(",")));
         vals = vals.substr(vals.find(",")+1);
     }
-    cout<<"B[ ";
-    for(int i = 0;i<4;i++){
-        cout<<reg[i]<<" ";
-    }cout<<"]\n";
+    // cout<<"B[ ";
+    // for(int i = 0;i<4;i++){
+    //     cout<<reg[i]<<" ";
+    // }cout<<"]\n";
 
-    int expected[4] = {-1,-1,-1,-1};
+    string cmdRaw = set[1];
+    vector<int> cmd(4);
+    cmd[0] = stoi(cmdRaw.substr(0,cmdRaw.find(" ")));
+    cmdRaw = cmdRaw.substr(cmdRaw.find(" ")+1);
+    cmd[1] = stoi(cmdRaw.substr(0,cmdRaw.find(" ")));
+    cmdRaw = cmdRaw.substr(cmdRaw.find(" ")+1);
+    cmd[2] = stoi(cmdRaw.substr(0,cmdRaw.find(" ")));
+    cmdRaw = cmdRaw.substr(cmdRaw.find(" ")+1);
+    cmd[3] = stoi(cmdRaw);
+
+    vector<int> expected(4);
     vals = set[2];
     vals = vals.substr(9);
     vals[vals.size()-1] = ',';
@@ -34,16 +47,19 @@ int valid(vector<string> set){
         expected[i] = stoi(vals.substr(0,vals.find(",")));
         vals = vals.substr(vals.find(",")+1);
     }
-    cout<<"->A[ ";
-    for(int i = 0;i<4;i++){
-        cout<<expected[i]<<" ";
-    }cout<<"]\n";
-
+    // cout<<"->A[ ";
+    // for(int i = 0;i<4;i++){
+    //     cout<<expected[i]<<" ";
+    // }cout<<"]\n";
+    for(int i = 0;i<16;i++){
+        out+=workingOpCode(reg,cmd,expected, i);
+    }
     return out;
 }
 
 void p1(){
-    cout<<valid(vector<string>{"Before: [1, 1, 1, 0]","4 1 0 0","After:  [1, 1, 1, 0]"})<<endl;
+    // cout<<valid(vector<string>{"Before: [3, 2, 1, 1]","9 2 1 2","After:  [3, 2, 2, 1]"})<<endl;
+    // return;
     vector<int> vals;
     string line;
     fstream file("p1inp.txt");
@@ -57,5 +73,12 @@ void p1(){
             vals.push_back(valid(temp));
         }
     }
-    cout<<vals.size()<<endl;
+    int threeOrMore = 0;
+    cout<<"[ ";
+    for(int i = 0;i<vals.size();i++){
+        cout<<vals[i]<<" ";
+        threeOrMore+=vals[i]>=3;
+    }
+    cout<<"]"<<endl;
+    cout<<threeOrMore<<" is answer for part 1."<<endl;
 }
